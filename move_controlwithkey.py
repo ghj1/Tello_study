9cb12c8369d8a31c9aaf6b90140ff8a5bd4df0a7
ghj1 <ghj12222@gmail.com>
2021-11-30 11:47:24 +0900

    main

---
+++ move_controlwithkey.py
@@ -0,0 +1,24 @@
+from djitellopy import Tello 
+
+
+tello = Tello()
+
+tello.connect()
+
+
+
+# move
+# tello.takeoff()
+# tello.move_up(110)
+# tello.flip_forward(30)
+# tello.move_forward(100)
+# tello.rotate_clockwise(90)
+# tello.move_forward(120)
+# tello.rotate_clockwise(90)
+
+
+
+
+# tello.land()
+
+pass
---
+++ move_plan.py
@@ -0,0 +1,22 @@
+from djitellopy import Tello 
+
+tello = Tello()
+
+tello.connect()
+
+tello.takeoff()
+
+# move
+tello.move_up(110)
+tello.flip_forward(30)
+# tello.move_forward(100)
+# tello.rotate_clockwise(90)
+# tello.move_forward(120)
+# tello.rotate_clockwise(90)
+
+
+
+
+tello.land()
+
+pass
