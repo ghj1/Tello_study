929fa5de0491cae5e8f3f5f060140ae559196d42
ghj1 <ghj12222@gmail.com>
2021-11-30 12:26:21 +0900

    main

--- move_controlwithkey.py
+++ move_controlwithkey.py
@@ -21,4 +21,32 @@ tello.connect()
 
 # tello.land()
 
+import cv2
+panel = cv2.imread('./tello.jpg')
+cv2.imshow('tello panel', panel)
+while True:
+    key = cv2.waitKey(1)
+    if key == ord('q'):
+        break
+    elif key == ord('t'):
+        tello.takeoff()
+    elif key == ord('l'):
+        tello.land()
+    elif key == ord('b'):
+        tello.move('back', 60)
+    elif key == ord('f'):
+        tello.move('forward', 60)
+    elif key == ord('u'):
+        tello.move('up', 110)
+    elif key == ord('d'):
+        tello.move('down', 30)
+    elif key == ord('w'):
+        tello.rotate_clockwise(90)
+    elif key == ord('r'):
+        tello.move('right', 20)
+    elif key == ord('l'):
+        tello.move('left', 20)
+    elif key == ord('pf'):
+        tello.flip_forward(20)
+
 pass
